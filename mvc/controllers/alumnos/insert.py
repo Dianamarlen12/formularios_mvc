3fd8ce36d905c947b2ce4bbf79785128b8a08f62
Dianamarlen12 <50559771+Dianamarlen12@users.noreply.github.com>
2020-07-31 01:57:02 -0500

    correcto

--- mvc/controllers/alumnos/insert.py
+++ mvc/controllers/alumnos/insert.py
@@ -1,5 +1,9 @@
 import web
 
+import mvc.models.personas as alumnos
+
+model_alumnos = alumnos.Alumnos()
+
 render = web.template.render("mvc/views/alumnos/")
 
 class Insert():
@@ -9,10 +13,19 @@ class Insert():
             return render.insert() # renderizando formulario.html
         except Exception as e:
             return "Error " + str(e.args)
-
     def POST(self):
         try:
             form = web.input()
-            print(form)
+            matricula = form.matricula
+            nombre = form.nombre
+            apellido_paterno = form.apellido_paterno
+            apellido_materno = form.apellido_materno
+            edad = form.edad
+            fecha_nacimiento = form.fecha_nacimiento
+            sexo = form.sexo
+            estado_civil = form.estado_civil
+            model_alumnos.insert(matricula, nombre, apellido_paterno, apellido_materno, edad, fecha_nacimiento, sexo, estado_civil)
+            web.seeother('/list')
         except Exception as e:
-            return "Error"
+            print(e)
+            return render.insert()
