89b645338377deb0d39e07ed2de2d5112b13ad60
Dianamarlen12 <50559771+Dianamarlen12@users.noreply.github.com>
2020-08-16 18:39:10 -0500

    correcto

--- sql/remoto.sql
+++ sql/remoto.sql
@@ -1,19 +1,16 @@
-CREATE DATABASE IF NOT EXISTS r41eax92x64eqwz5;
 USE r41eax92x64eqwz5;
-SELECT DATABASE();
 
-DROP TABLE IF EXISTS alumnos;
 CREATE TABLE IF NOT EXISTS alumnos(
 id_alumno	     		INT(4)          	NOT NULL        AUTO_INCREMENT          PRIMARY KEY,
-matricula               CHAR(10)     		NOT NULL,
-nombre           		VARCHAR(30)    		NOT NULL,
-apellido_paterno		VARCHAR(20)			NOT NULL,
+matricula               CHAR(10)            NOT NULL,
+nombre           		VARCHAR(30)        NOT NULL,
+apellido_paterno        VARCHAR(20)	        NOT NULL,
 apellido_materno        VARCHAR(20)         NOT NULL,
-edad					CHAR(2)				NOT NULL,
-fecha_nacimiento		DATE,
-sexo					CHAR(10)			NOT NULL,
-estado_civil			CHAR(10)			NOT NULL
-)ENGINE= InnoDB DEFAULT CHARSET=latin1;
+edad                    CHAR(2)	            NOT NULL,
+fecha_nacimiento        DATE,
+sexo                    CHAR(10)            NOT NULL,
+estado_civil            CHAR(10)            NOT NULL
+) ENGINE= InnoDB DEFAULT CHARSET=latin1;
 
 /*INSERCION DE DOS ALUMNOS*/
 INSERT INTO alumnos(id_alumno, matricula, nombre, apellido_paterno, apellido_materno, edad, fecha_nacimiento, sexo, estado_civil) VALUES 
